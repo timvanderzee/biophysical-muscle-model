bedb4e5781f02666d13773d569d002c571bbac2d
Tim travel <timvdzee@hotmail.com>
2026-02-12 16:04:02 +0100

    update get_paths

--- get_paths.m
+++ get_paths.m
@@ -1,15 +1,28 @@
-function[datafolder, modelfolder, githubfolder] = get_paths()
+function[datafolder, outputfolder, githubfolder] = get_paths()
     
-% this datafolder should contain the subfolders 2017 and 2018
-datafolder = '';
-
-% this folder is where the model fits are or will be saved
-modelfolder = '';
-
-% this folder should contain the biophysical-muscle-model repository
-githubfolder = '';
+%% github folder
+% githubfolder should be a string specifying the location of the folder
+% containing the biophysical-muscle-model repository. It depends on where
+% you cloned or downloaded the repository to
+githubfolder = ''; % e.g. C:\John_Doe\Documents\github
 
 % add the model functions to the path
 addpath(genpath([githubfolder, '\biophysical-muscle-model\Common\']))
+
+%% output folder
+% datafolder should be a string specifying the location of the folder where
+% you would like to export the simulation output to. It can be any folder
+% on your device.
+outputfolder = ''; % e.g. C:\John_Doe\Documents\simulation_output
+
+%% data folder
+% datafolder should be a string specifying the location of the folder
+% containing the experimental data. Note: at this time, data is provided to
+% reviewers only. Reviewers will see the data as supplemental information
+% to the submitted manuscript. Two subfolders are provided as ZIP files,
+% named '2017' and '2018'. datafolder should specify the folder that
+% contains both these (unzipped) subfolders. If you are not a reviewer and
+% do not have the data, you do not have to change the line below
+datafolder = ''; % e.g. C:\John_Doe\Documents\data
 
 end
